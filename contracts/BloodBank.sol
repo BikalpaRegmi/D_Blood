deaa2bd1d9512f76d2e2a2cf5ece8a4c3032a5d0
BikalpaRegmi <Bikalparegmihtd3@gmail.com>
2024-10-20 13:56:52 +0545

    make it better

--- contracts/BloodBank.sol
+++ contracts/BloodBank.sol
@@ -9,7 +9,7 @@ contract BLOODBANK {
     uint totalBKSSupply = 1000000 ;
 
     struct LoginDetails{
-        string id ;
+        address id ;
        string name ;
        string bloodType;
        string dateOfBirth;
@@ -18,25 +18,35 @@ contract BLOODBANK {
        uint emergencyContact ;
     }
 
+    struct Comment {
+        address commentator ;
+        string text ;
+    }
+
     struct Requests {
+        address requestor;
         string requestId;
         string details ;
         string image ; 
-        string comments;
+        Comment[] comments;
     }
 
-    mapping(string=>LoginDetails) public profile ;
+     //Profiles
+    mapping(address=>LoginDetails) public profile ;
 
+    //Requests
     mapping(string=>Requests) public allRequests ;
+    string[] public requestIds;
     mapping(address=>uint) public myRequestsCount ;
     mapping(address=>mapping(uint=>Requests)) public myRequests ;
-    string[] public requestIds;
-
+   
+   //BKS tokens
     mapping(address=>uint) public balances ;
     
+    //events
     event CreateProfile (string name , string bloodType );
     event CreateRequestEvent (string timestamp , string details );
-
+    event AddComment(string requestId , string comment);
 
     modifier onlyOwner(){
         require(msg.sender ==owner , "This function can only be run if you are owner");
@@ -45,36 +55,157 @@ contract BLOODBANK {
 
 
     constructor(){
-        name = "Blood Bank";
+        name = "D Blood";
         owner = msg.sender;
         balances[msg.sender] = totalBKSSupply;
     }
     
-    function LoginUser(string memory _id , string memory _name , string memory _bloodType , string memory _dob , string memory _gender , string memory _MR , uint  _EC) external{
+    function LoginUser( string memory _name , string memory _bloodType , string memory _dob , string memory _gender , string memory _MR , uint  _EC) external{
 
-     LoginDetails memory loginDetails = LoginDetails(_id , _name , _bloodType , _dob , _gender , _MR , _EC);
+     LoginDetails memory loginDetails = LoginDetails(msg.sender , _name , _bloodType , _dob , _gender , _MR , _EC);
 
-      profile[_id] = loginDetails;
+      profile[msg.sender] = loginDetails;
 
       emit CreateProfile(_name , _bloodType);
 
     }
 
-    function buyBks() external payable {
-    require(msg.sender !=owner , "Owner Cant buy tokens");
-    require(msg.value == 3 * 10**15, "plz pay 0.003 eth");
-    balances[msg.sender]+=100 ;
+    function EditMyProfile( string memory _name , string memory _bloodType , string memory _dob , string memory _gender , string memory _MR , uint  _EC) external{
+     LoginDetails storage loginDetails = profile[msg.sender];
+
+     require(loginDetails.id == msg.sender , "You are not the profile owner");
+     
+
+    if (bytes(_name).length > 0) {
+        loginDetails.name = _name;
+    }
+    if (bytes(_bloodType).length > 0) {
+        loginDetails.bloodType = _bloodType;
+    }
+    if (bytes(_dob).length > 0) {
+        loginDetails.dateOfBirth = _dob;
+    }
+    if (bytes(_gender).length > 0) {
+        loginDetails.gender = _gender;
+    }
+    if (bytes(_MR).length > 0) {
+        loginDetails.medicalReport = _MR;
+    }
+    if (_EC != 0) {
+        loginDetails.emergencyContact = _EC;
+    }
+     
 
     }
 
     function CreateRequest (string memory _requestID , string memory _details , string memory _image) external {
      require(balances[msg.sender] >= 100 , "not enough BKS token") ;
-    Requests memory request = Requests(_requestID , _details , _image , ""); 
+
+    Requests storage request = allRequests[_requestID]; 
+    request.requestor = msg.sender;
+    request.requestId = _requestID ;
+    request.details = _details ;
+    request.image = _image ;
     allRequests[_requestID] = request ;
     requestIds.push(_requestID);
     myRequestsCount[msg.sender]++;
     myRequests[msg.sender][myRequestsCount[msg.sender]] = request;
+
     emit CreateRequestEvent(_requestID , _details);
     }
-//  aaba aayexi getallrequests wala function bana ani get allRequests wala functin pani bana return gar ani baki paxi herlas
+
+   function getAllRequests() external view returns(Requests[] memory){
+      Requests[] memory requests = new Requests[](requestIds.length);
+      
+      for(uint i =0 ; i<requestIds.length ; i++){
+        requests[i] = allRequests[requestIds[i]];
+      }
+
+      return requests ;
+   }
+
+   function getAllMyRequests() external view returns (Requests[] memory){
+     require(myRequestsCount[msg.sender] > 0 , "You have no Requests till now") ;
+      
+      uint count = myRequestsCount[msg.sender];
+
+      Requests[] memory myAllRequests = new Requests[](count);
+
+      for (uint i=0 ; i<count ; i++){
+       myAllRequests[i] = myRequests[msg.sender][i+1];
+      }
+
+      return myAllRequests ;
+   }
+
+    function addComment(string memory _requestId, string memory _comment) external {
+        require(bytes(allRequests[_requestId].requestId).length != 0 , "The Request Doesn't Exists");  //bytes is required to check the length of the string u cant directly access the length in solidity by directly using string.length the bytes will change the single letters of string into hexadecimal or ascii and if there are no string letter then it will return 0x000 and if there is "ab" then it will return 0x6162. 
+        Requests storage request = allRequests[_requestId];   //we are using storage here cuz we are updating state inside a mapping to push somethin on array we could use memory but if we are playing with a state in a mapping or trying to change a state we need storage.
+        
+
+
+        Comment memory comment =  Comment({ //we are using memory here cuz we are pushing an obj
+          commentator:msg.sender ,
+          text:_comment
+        });
+
+        request.comments.push(comment);
+    }
+
+ function removeComment(string memory _requestId) external {
+    require(bytes(allRequests[_requestId].requestId).length != 0, "Request doesn't exist");
+    
+    Requests storage request = allRequests[_requestId];
+    uint indexToRemove = type(uint).max; 
+    bool exists = false;
+
+    for (uint i = 0; i < request.comments.length; i++) {
+        if (request.comments[i].commentator == msg.sender) {
+            exists = true;
+            indexToRemove = i; // Store the index of the comment to remove
+            break;
+        }
+    }
+
+    require(exists == true, "Your comment doesn't exist");
+
+    // Replace the comment to remove with the last comment and pop
+    if (indexToRemove < request.comments.length) {
+        request.comments[indexToRemove] = request.comments[request.comments.length - 1];
+        request.comments.pop(); // Remove the last comment
+    }
+}
+
+function getComments(string memory _id) external view returns(Comment[] memory){
+  return allRequests[_id].comments ;
+}
+
+function transferBKS(address _id , uint _amount) external {
+    require(balances[msg.sender] > 0 , "You dont have any tokens");
+    require(balances[msg.sender] >= _amount , "You dont have enough tokens to send BKS");
+
+    balances[_id] += _amount;
+    balances[msg.sender] -= _amount;
+}
+
+function withdraw() external payable{
+    require(balances[msg.sender] > 0 , "You dont have any BKS to withdraw");
+    uint amountToWithdraw = balances[msg.sender] * 0.00002 ether ;
+ payable(msg.sender).transfer(amountToWithdraw);
+  balances[msg.sender] = 0;
+}
+
+function buyBks() external payable {
+    require(msg.sender !=owner , "Owner Cant buy tokens");
+    require(msg.value == 0.001 ether || msg.value == 0.002 ether, "You should pay 0.001 eth or 0.002 eth to buy BKS");
+
+     if(msg.value == 0.002 ether) {
+      balances[msg.sender] += 100;
+     } 
+
+     if(msg.value == 0.001 ether) {
+      balances[msg.sender] += 50;
+     }  
+    }
+
 }
--- test/BloodBank.ts
+++ test/BloodBank.ts
@@ -6,26 +6,24 @@ import { BLOODBANK } from "../typechain-types";
 describe("BloodBank", () => {
   let owner: Signer, addr1: Signer, addr2: Signer, contract: BLOODBANK;
   let transaction: any;
-
-  const Id: string = Date.now().toString();
+  
   const name: string = "Bikalpa";
   const bloodType: string = "B (+ve)";
   const dateOfBirth: string = "04-07-2006";
   const gender: string = "male";
   const medicalReport: string =
-    "https://images.drlogy.com/assets/uploads/lab/image/cbc-test-report-format-example-sample-template-drlogy-lab-report.webp";
-
-const emergencyContact: number = 9899999999;
-
+  "https://images.drlogy.com/assets/uploads/lab/image/cbc-test-report-format-example-sample-template-drlogy-lab-report.webp";
+  
+  const emergencyContact: number = 9899999999;
+  
+  
   beforeEach(async () => {
     const contractFactory: any = await ethers.getContractFactory("BLOODBANK");
     [owner, addr1, addr2] = await ethers.getSigners();
     contract = await contractFactory.deploy();
-    transaction = await contract.connect(addr1).LoginUser(Id, name, bloodType, dateOfBirth, gender, medicalReport , emergencyContact);
+    transaction = await contract.connect(addr1).LoginUser( name, bloodType, dateOfBirth, gender, medicalReport , emergencyContact);
   });
-
-   
-
+  
  
   
   describe("Deployment",  () => {
@@ -34,7 +32,7 @@ const emergencyContact: number = 9899999999;
     });
 
     it("Should assign right name", async () => {
-      expect(await contract.name()).to.eq("Blood Bank")
+      expect(await contract.name()).to.eq("D Blood")
     })
   });
 
@@ -44,19 +42,28 @@ const emergencyContact: number = 9899999999;
     });
 
     it("Should provide correct profile", async () => {
-      const profile = await contract.profile(Id);
+      const profile: any = await contract.profile(addr1);
 
-      expect(profile.id).to.eq(Id);
+      expect(profile.id).to.eq(addr1);
       expect(profile.name).to.eq(name);
       expect(profile.dateOfBirth).to.eq(dateOfBirth);
       expect(profile.bloodType).to.eq(bloodType);
       expect(profile.gender).to.eq(gender);
       expect(profile.medicalReport).to.eq(medicalReport);
       expect(profile.emergencyContact).to.eq(emergencyContact);
+    });
+
+    it("Should edit the profiles", async () => {
+      const changedGender = "Female"
+      await contract.connect(addr1).EditMyProfile("", "", "",changedGender , "", 0);
+      const profile = await contract.profile(addr1);
+
+      expect(profile.name).to.eq(name);
+      expect(profile.gender).to.eq(changedGender);
     })
   });
 
-  describe("BuyBKS", () => {
+  describe("BKS", () => {
     it("Should not let owner buy BKS", async () => {
       await (
         expect(
@@ -65,20 +72,34 @@ const emergencyContact: number = 9899999999;
       );
     });
 
-    it("Should the transaction be exact 0.003eth", async () => {
-      await (
-        expect(contract.connect(addr1).buyBks({ value: ethers.parseEther("0.004") }))
-      ).to.be.revertedWith("plz pay 0.003 eth");
+    it("Should the transaction be exact 0.001eth or 0.002eth", async () => {
+      await(
+        expect(
+          contract.connect(addr1).buyBks({ value: ethers.parseEther("0.004") })
+        )
+      ).to.be.revertedWith("You should pay 0.001 eth or 0.002 eth to buy BKS");
     });
 
     it("Should increase the balance of buyer", async () => {
       const initialBalance = await contract.balances(addr2);
 
-      await contract.connect(addr2).buyBks({ value: ethers.parseEther("0.003") });
+      await contract.connect(addr2).buyBks({ value: ethers.parseEther("0.002") });
+      await contract.connect(addr1).buyBks({ value: ethers.parseEther("0.001") });
 
       expect(await contract.balances(addr2)).to.eq(Number(initialBalance) + 100);
+      expect(await contract.balances(addr1)).to.eq(Number(initialBalance) + 50);
+    });
+
+    it("Should withdraw", async () => {
+            await contract
+              .connect(addr2)
+              .buyBks({ value: ethers.parseEther("0.002") });
+      const balanceBefore = await ethers.provider.getBalance(addr2);
+      await contract.connect(addr2).withdraw();
+            const balanceAfter = await ethers.provider.getBalance(addr2);
+      expect(balanceAfter).to.be.greaterThan(balanceBefore);
     })
-  })
+  });
 
   describe("Requests", () => {
     const Id: string = Date.now.toString();
@@ -86,10 +107,15 @@ const emergencyContact: number = 9899999999;
     const image: string = "image.jpeg";
     
     beforeEach(async () => {
-      await contract.connect(addr2).buyBks({ value: ethers.parseEther('0.003') });
+      await contract.connect(addr2).buyBks({ value: ethers.parseEther('0.002') });
       transaction = await contract.connect(addr2).CreateRequest(Id, detail, image);
       await transaction.wait();
     });
+
+    it("Should know who send the request", async () => {
+      const request = await contract.allRequests(Id);
+      expect(request.requestor).to.eq(addr2);
+    })
  
     it("Should the victim have more than 100 BKS", async () => {
       await expect(contract.connect(addr1).CreateRequest(Id, detail, image)).to.be.revertedWith("not enough BKS token");
@@ -115,13 +141,82 @@ const emergencyContact: number = 9899999999;
       expect(await contract.connect(addr2).myRequestsCount(addr2)).to.eq(1);
     });
 
-    it("Should map all of my requests", async () => {
+    it("Should show my requests", async () => {
       const myRequest = await contract.connect(addr2).myRequests(addr2, 1);
       expect(myRequest.requestId).to.eq(Id);
       expect(myRequest.details).to.eq(detail);
       expect(myRequest.image).to.eq(image);
-
     });
 
+    it("Should map all the requests created", async () => {
+      const res = await contract.getAllRequests();
+      expect(res[0].requestId).to.eq(Id);
+      expect(res[0].details).to.eq(detail);
+      expect(res[0].image).to.eq(image);
+    });
+
+    it("Should map all of my requests", async () => {
+      const res = await contract.connect(addr2).getAllMyRequests();
+      expect(res[0].requestId).to.eq(Id);
+      expect(res[0].details).to.eq(detail);
+      expect(res[0].image).to.eq(image);
+    });
+
+    it("Should revert if there is no request by an account", async () => {
+      const res = contract.connect(addr1).getAllMyRequests();
+      await expect(res).to.be.revertedWith("You have no Requests till now");
+    });
+
+    describe("comments",  () => {
+      it("Should revert if there is no request existing", async () => {
+        const length = contract.addComment("999", "hi");
+        expect(length).to.be.revertedWith("The Request Doesn't Exists");
+      });
+
+     it("Should push the address and comment", async () => {
+       await contract.connect(addr1).addComment(Id, "i will help");
+
+       const comment:any = await contract.getComments(Id);
+
+       expect(comment[0].commentator).to.eq(await addr1.getAddress());
+       expect(comment[0].text).to.eq("i will help");
+     });
+      
+      it("Should remove the comment", async () => {
+        await contract.connect(addr1).addComment(Id, "i am addr1 i will help");
+        await contract.connect(addr2).addComment(Id, "i am addr2 i will help");
+        await contract.connect(owner).addComment(Id, "i am owner i will help");
+        
+        await contract.connect(owner).removeComment(Id);
+
+        const comment:any = await contract.getComments(Id);
+
+        expect(comment.length).to.eq(2);
+        expect(comment[0].text).to.eq("i am addr1 i will help");
+        expect(comment[1].text).to.eq("i am addr2 i will help");
+      });
+
+      it("Should revert if not commented", async () => {
+        await expect(contract.removeComment(Id)).to.be.revertedWith(
+          "Your comment doesn't exist"
+        );
+      })
+      it("Should get all the comments", async () => {
+                await contract
+                  .connect(addr1)
+                  .addComment(Id, "i am addr1 i will help");
+                await contract
+                  .connect(addr2)
+                  .addComment(Id, "i am addr2 i will help");
+
+        const comments = await contract.getComments(Id);
+        expect(comments[0].text).to.eq("i am addr1 i will help");
+        expect(comments[1].commentator).to.eq(addr2);
+      })
+
   })
+
+  });
+
+ 
 })
